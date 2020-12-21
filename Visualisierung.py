2bb20da5527967b4e3e380fc40a70d498527ae7e
Keschmc <jun.heule@gmail.com>
2020-12-21 15:54:52 +0100

    Grundbild formatiert und mit Tastenfelder erstellt.
    //Stand mit GrundRoot

--- Visualisierung.py
+++ Visualisierung.py
@@ -3,7 +3,7 @@ from tkinter import *
 
 # Parameter für Buttons
 # Vorlage Button(root, text="", command=, width=width, height=height, justify=justify, padx=padx, pady=pady, font=font, bg=str(Color))
-BgColor = "#bfbeba"
+BgColor = "#fffefa"
 Color = "#6e6d68"
 ColorActive = "#ffd814"
 font = "Arial"
@@ -21,14 +21,20 @@ root.configure(bg=str(BgColor))
 
 # Tastenfeld rechts
 FeldRechts = LabelFrame(width=105, height=400, bg=str(BgColor))
-FeldRechts.grid(row=0, column=5, sticky=E)
+FeldRechts.grid(row=1, column=5, sticky=E)
 FeldRechts.grid_propagate(0)
 
 # Tastenfeld unten
 FeldUnten = LabelFrame(width=800, height=80, bg=str(BgColor))
-FeldUnten.grid(row=5, column=0)
+FeldUnten.grid(row=5, column=0, columnspan=6, sticky= W+E)
+FeldUnten.grid_propagate(0)
+
+# Projekt Titel
+Titel = Label(root, text= "RaspyPool; IDPA Projekt 2020", relief= RAISED)
+Titel.grid(row=0, column=0, columnspan=6, sticky= W+E)
 
 # Commands
+# Tastenfeld rechts
 def Auto():
     pass
 
@@ -46,6 +52,16 @@ def Start():
 
 
 def Stopp():
+    pass
+
+#Tastenfeld unten
+def Pump():
+    pass
+
+def Filter():
+    pass
+
+def Heat():
     pass
 
 
@@ -73,6 +89,20 @@ BtnService.grid(row=2, column=0, pady=5)
 BtnStart.grid(row=3, column=0, pady=5)
 BtnStopp.grid(row=4, column=0, pady=5)
 
+#Tastenfeld unten
+BtnPump = Button(FeldUnten, text="Pumpe", command=Pump, width=width, height=height, justify=justify, font=font,
+                 bg=str(Color))
+
+BtnFilter =Button(FeldUnten, text="Filter", command=Filter, width=width, height=height, justify=justify, font=font,
+                 bg=str(Color))
+
+BtnHeat =Button(FeldUnten, text="Heizung", command=Heat, width=width, height=height, justify=justify, font=font,
+                 bg=str(Color))
+
+# Ordne Buttons ins Tastenfeld unten
+BtnPump.grid(row=0, column=0, padx=10)
+BtnFilter.grid(row=0, column=1, padx=10)
+BtnHeat.grid(row=0, column=2, padx=10)
 
 # Mainloop
 root.mainloop()
