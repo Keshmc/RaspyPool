7d6f44bb2c353f66bcaf03dae2b7090311e46607
Keschmc <jun.heule@gmail.com>
2020-12-21 16:48:43 +0100

    Für jede "Station einzelne Bilder eingefügt"
    //zum veranschaulichen mit Farbe hinterlegt

--- Visualisierung.py
+++ Visualisierung.py
@@ -19,6 +19,10 @@ root.title("Raspy Pool")
 root.geometry("800x480")
 root.configure(bg=str(BgColor))
 
+# Projekt Titel
+Titel = Label(root, text="RaspyPool; IDPA Projekt 2020", relief=RAISED)
+Titel.grid(row=0, column=0, columnspan=6, sticky=W + E)
+
 # Tastenfeld rechts
 FeldRechts = LabelFrame(width=105, height=400, bg=str(BgColor))
 FeldRechts.grid(row=1, column=5, sticky=E)
@@ -26,12 +30,9 @@ FeldRechts.grid_propagate(0)
 
 # Tastenfeld unten
 FeldUnten = LabelFrame(width=800, height=80, bg=str(BgColor))
-FeldUnten.grid(row=5, column=0, columnspan=6, sticky= W+E)
+FeldUnten.grid(row=5, column=0, columnspan=6, sticky=W + E)
 FeldUnten.grid_propagate(0)
 
-# Projekt Titel
-Titel = Label(root, text= "RaspyPool; IDPA Projekt 2020", relief= RAISED)
-Titel.grid(row=0, column=0, columnspan=6, sticky= W+E)
 
 # Commands
 # Tastenfeld rechts
@@ -54,15 +55,37 @@ def Start():
 def Stopp():
     pass
 
-#Tastenfeld unten
+
+# Tastenfeld unten
+def Home():
+    PicHome = LabelFrame(bg=str(BgColor), width=(800 - 105), height=400)
+    PicHome.grid(row=1, column=0, rowspan=4, columnspan=4)
+    PicHome.grid_propagate(0)
+
 def Pump():
-    pass
+    PicPump = LabelFrame(bg=str("#84ab8d"), width=(800 - 105), height=400)
+    PicPump.grid(row=1, column=0, rowspan=4, columnspan=4)
+    PicPump.grid_propagate(0)
+
+
 
 def Filter():
-    pass
+    PicFilter = LabelFrame(bg=str("#cb34d9"), width=(800 - 105), height=400)
+    PicFilter.grid(row=1, column=0, rowspan=4, columnspan=4)
+    PicFilter.grid_propagate(0)
+
 
 def Heat():
-    pass
+    PicHeat = LabelFrame(bg=str("#d95d34"), width=(800 - 105), height=400)
+    PicHeat.grid(row=1, column=0, rowspan=4, columnspan=4)
+    PicHeat.grid_propagate(0)
+
+
+def Parameter():
+    # Bild Parameter
+    PicParameter = LabelFrame(bg=str("#427bf5"), width=(800 - 105), height=400)
+    PicParameter.grid(row=1, column=0, rowspan=4, columnspan=4)
+    PicParameter.grid_propagate(0)
 
 
 # Tastenleiste rechts
@@ -89,20 +112,28 @@ BtnService.grid(row=2, column=0, pady=5)
 BtnStart.grid(row=3, column=0, pady=5)
 BtnStopp.grid(row=4, column=0, pady=5)
 
-#Tastenfeld unten
+# Tastenfeld unten
+BtnHome = Button(FeldUnten, text="Home", command=Home, width=width, height=height, justify=justify, font=font,
+                 bg=str(Color))
 BtnPump = Button(FeldUnten, text="Pumpe", command=Pump, width=width, height=height, justify=justify, font=font,
                  bg=str(Color))
 
-BtnFilter =Button(FeldUnten, text="Filter", command=Filter, width=width, height=height, justify=justify, font=font,
+BtnFilter = Button(FeldUnten, text="Filter", command=Filter, width=width, height=height, justify=justify, font=font,
+                   bg=str(Color))
+
+BtnHeat = Button(FeldUnten, text="Heizung", command=Heat, width=width, height=height, justify=justify, font=font,
                  bg=str(Color))
 
-BtnHeat =Button(FeldUnten, text="Heizung", command=Heat, width=width, height=height, justify=justify, font=font,
-                 bg=str(Color))
+BtnParameters = Button(FeldUnten, text="Parameter", command=Parameter, width=width, height=height, justify=justify,
+                       font=font,
+                       bg=str(Color))
 
 # Ordne Buttons ins Tastenfeld unten
-BtnPump.grid(row=0, column=0, padx=10)
-BtnFilter.grid(row=0, column=1, padx=10)
-BtnHeat.grid(row=0, column=2, padx=10)
+BtnHome.grid(row=0, column=0, padx=10, pady=10)
+BtnPump.grid(row=0, column=0, padx=10, pady=10)
+BtnFilter.grid(row=0, column=1, padx=10, pady=10)
+BtnHeat.grid(row=0, column=2, padx=10, pady=10)
+BtnParameters.grid(row=0, column=3, padx=10, pady=10)
 
 # Mainloop
 root.mainloop()
